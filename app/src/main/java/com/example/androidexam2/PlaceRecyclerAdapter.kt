0be5e828d36686541386b43b62d6c898bddf5936
KT <thun.kristian@gmail.com>
2023-12-18 13:43:25 +0100

    Started recyclerView for places

--- app/src/main/java/com/example/androidexam2/PlaceRecyclerAdapter.kt
+++ app/src/main/java/com/example/androidexam2/PlaceRecyclerAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.androidexam2
 
-class PlaceRecyclerAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PlaceRecyclerAdapter(context: Context, val places: MutableList<Place>): RecyclerView.Adapter<PlaceRecyclerAdapter.ItemViewHolder>() {
+    inner class ItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val placeNameTextView = itemView.findViewById<TextView>(R.id.placeNameTextView)
+    }
+
+    val inflater = LayoutInflater.from(context)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount() = places.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
