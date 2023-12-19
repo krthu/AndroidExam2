30406e5ecda1e88659e51c3f388f89fcd4cbd42a
KT <thun.kristian@gmail.com>
2023-12-19 09:45:06 +0100

    Added Glide for downloading images

--- app/src/main/java/com/example/androidexam2/PlaceRecyclerAdapter.kt
+++ app/src/main/java/com/example/androidexam2/PlaceRecyclerAdapter.kt
@@ -2,16 +2,18 @@ package com.example.androidexam2
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.firebase.Firebase
 import com.google.firebase.storage.FirebaseStorage
 
-class PlaceRecyclerAdapter(context: Context, val places: MutableList<Place>): RecyclerView.Adapter<PlaceRecyclerAdapter.ItemViewHolder>() {
+class PlaceRecyclerAdapter(val context: Context, val places: MutableList<Place>): RecyclerView.Adapter<PlaceRecyclerAdapter.ItemViewHolder>() {
     inner class ItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         val placeNameTextView = itemView.findViewById<TextView>(R.id.placeNameTextView)
         val rowPlaceImageView = itemView.findViewById<ImageView>(R.id.rowPlaceImageView)
@@ -33,12 +35,18 @@ class PlaceRecyclerAdapter(context: Context, val places: MutableList<Place>): Re
 
         holder.placeNameTextView.text = place.name
         val storageRef = place.imageURI?.let { FirebaseStorage.getInstance().getReferenceFromUrl(it) }
-        val ONE_MEGABYTE: Long = 1024 * 1024
-        storageRef?.getBytes(ONE_MEGABYTE)
-            ?.addOnSuccessListener {bytes ->
-                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                holder.rowPlaceImageView.setImageBitmap(bitmap)
-            }
+        Log.d("!!!", storageRef.toString())
+        storageRef?.downloadUrl?.addOnSuccessListener { uri ->
+            Glide.with(context)
+                .load(uri)
+                .into(holder.rowPlaceImageView)
+        }
+//        val ONE_MEGABYTE: Long = 1024 * 1024
+//        storageRef?.getBytes(ONE_MEGABYTE)
+//            ?.addOnSuccessListener {bytes ->
+//                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+//                holder.rowPlaceImageView.setImageBitmap(bitmap)
+//            }
 
 
 
